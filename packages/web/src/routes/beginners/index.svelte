1541d5099f75ac22768192dd91ca93ccc531453e
Brian Takita <brian.takita@gmail.com>
2019-05-05 18:55:17 -0400

    packages/ctx-core
    packages/web/package.json
    
    	↑ "@ctx-core/markdown": "^2.1.0"
    
    packages/web/src/routes/beginners/index.svelte
    
    	+ a1__content__md
    		- a1__content__md__dir
    
    packages/web/src/routes/build/install/index.svelte
    
    	+ a1__name
    	+ a1__content__md
    		- a1__content__md__dir

--- packages/web/src/routes/beginners/index.svelte
+++ packages/web/src/routes/beginners/index.svelte
@@ -2,21 +2,21 @@
 	export async function preload({ query, params }) {
 		const response = await this.fetch('/beginners.json')
 		const json = await response.json()
-		const { a1__content__md__dir } = json
+		const { a1__content__md } = json
 		return {
-			a1__content__md__dir
+			a1__content__md
 		}
 	}
 </script>
 
 <script>
 	import A__Edit from '@holochain-developer-prototype/web/src/layout/A__Edit.svelte'
-	export let a1__content__md__dir
+	export let a1__content__md
 </script>
 
 <h1>👋 Welcome</h1>
 
-{#each a1__content__md__dir as content__md__dir}
+{#each a1__content__md as content__md__dir}
 	{@html content__md__dir.html}
 {/each}
 
--- packages/web/src/routes/build/install/index.svelte
+++ packages/web/src/routes/build/install/index.svelte
@@ -2,22 +2,24 @@
 	export async function preload({ query, params }) {
 		const response = await this.fetch('/build/install.json')
 		const json = await response.json()
-		const { a1__content__md__dir } = json
+		const { a1__name, a1__content__md } = json
 		return {
-			a1__content__md__dir
+			a1__name,
+			a1__content__md
 		}
 	}
 </script>
 
 <script>
 	import A__Edit from '@holochain-developer-prototype/web/src/layout/A__Edit.svelte'
-	export let a1__content__md__dir
+	export let a1__name
+	export let a1__content__md
 </script>
 
 <h1>Install Holochain</h1>
 
-{#each a1__content__md__dir as content__md__dir}
-	{@html content__md__dir.html}
+{#each a1__content__md as content__md,i}
+	{@html content__md.html}
+	<A__Edit href="/build/install/_content/{a1__name[i]}.md"></A__Edit>
 {/each}
 
-<A__Edit href="/build/install/_content"></A__Edit>
