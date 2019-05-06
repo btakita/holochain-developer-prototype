6a6cbd3eacb8efc8ff6be423de6c83598765fb6b
Brian Takita <brian.takita@gmail.com>
2019-05-06 09:47:41 -0400

    feat(holochain-developer-prototype/web): GET /beginners: edit link to each markdown file in _content

--- packages/web/src/routes/beginners/index.svelte
+++ packages/web/src/routes/beginners/index.svelte
@@ -2,8 +2,9 @@
 	export async function preload({ query, params }) {
 		const response = await this.fetch('/beginners/json')
 		const json = await response.json()
-		const { a1__content__md } = json
+		const { a1__name, a1__content__md } = json
 		return {
+			a1__name,
 			a1__content__md
 		}
 	}
@@ -11,13 +12,13 @@
 
 <script>
 	import A__Edit from '@holochain-developer-prototype/web/src/layout/A__Edit.svelte'
+	export let a1__name
 	export let a1__content__md
 </script>
 
 <h1>👋 Welcome</h1>
 
-{#each a1__content__md as content__md__dir}
+{#each a1__content__md as content__md__dir, i}
 	{@html content__md__dir.html}
+	<A__Edit href="/beginners/_content/{a1__name[i]}.md"></A__Edit>
 {/each}
-
-<A__Edit href="/beginners/_content"></A__Edit>
